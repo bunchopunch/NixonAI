3a73e05159b0718ae1d980fa1608c4cabd671451
bunchopunch <bunchopunch@mustacheforpresident.com>
2014-12-03 01:56:33 -0700

    Moving cleaing of input to submit function

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,14 @@ $(document).ready(function() {
   apiUrl = 'http://www.pandorabots.com/pandora/talk-xml';
 
   var submitForm = function(){
+    var currentOutgoingMessage = inputElement.val().toString();
+    $('#inputField').val("");
+    outputTarget.append('<p class="user">' + currentOutgoingMessage + '</p>');
     if(inputElement.val().length === 0){
       return false
     } else {
       $.post( apiUrl, 
-        {botid: bot, input: $('#inputField').val(), custid: customer},
+        {botid: bot, input: currentOutgoingMessage, custid: customer},
         function() {})
         .done(function(data) {
           var content = $(data).find( "that" ).text(); // Get the text out of the XML response
@@ -26,8 +29,6 @@ $(document).ready(function() {
 
   var showResponse = function(responsePhrase){
     var outputTarget = $('#outputField');
-    outputTarget.append('<p class="user">' + $('#inputField').val() + '</p>');
-    $('#inputField').val("");
     outputTarget.append('<p>' + responsePhrase + '</p>');
     outputTarget.animate({ scrollTop: outputTarget.attr("scrollHeight") - outputTarget.height() }, 500);
   }
